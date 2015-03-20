c4da8eb12e75a9903d1310fcae83c194c49f8db6
Shinpei Noda <noda@haw.co.jp>
2015-03-20 23:05:55 +0900

    :new: 動画の追加機能を実装した。

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,24 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def create
+    begin
+      movie = Movie.create_by_video_id(create_params[:video_id])
+    rescue ActiveRecord::RecordInvalid => e
+      flash[:error] = e.message
+      redirect_to(action: :index)
+      return
+    end
+
+    flash[:notice] = "「#{movie.title}」を追加しました。"
+
+    redirect_to(action: :index)
+  end
+
+  private
+
+  def create_params
+    params.require(:movie).permit(:video_id)
+  end
 end
